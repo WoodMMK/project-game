c77961515f36975553d110dcc5419391deee4fa6
thaoza12345 <rapeepat.bol@student.mahidol.edu>
2024-10-09 17:16:10 +0700

    already add bg

--- src/main/java/Gamecode/GamePanel.java
+++ src/main/java/Gamecode/GamePanel.java
@@ -190,35 +190,35 @@ public class GamePanel extends JPanel {
     }
     
     public void paintComponent(Graphics g){
-        super.paintComponent(g); 
-         if (Bg01 != null) {
-            g.drawImage(Bg01, 0, -1150,2100,2000, null);
+        super.paintComponent(g);
+        int cameraX = xPos - 640;
+        int cameraY = yPos - 360;
+          if (Bg01 != null) {
+            g.drawImage(Bg01,(cameraX-700)/30,-1400,2400,2400, null);
         }
          if (Bg02 != null) {
-             g.drawImage(Bg02, 0,-1150, 2100,2000, null);
+            g.drawImage(Bg02,(cameraX-700)/27,-1400,2400,2400, null);
         }
          if (Bg03 != null) {
-            g.drawImage(Bg03, 0,-1150, 2100,2000, null);
+            g.drawImage(Bg03,(cameraX-700)/25,-1400,2400,2400, null);
         }
          if (Bg04 != null) {
-            g.drawImage(Bg04, 0,-1150,2100,2000, null);
+            g.drawImage(Bg04,(cameraX-700)/20,-1400,2400,2400, null);
         }
          if (Bg05 != null) {
-            g.drawImage(Bg05,0,-1150,2100,2000, null);
+            g.drawImage(Bg05,(cameraX-700)/10,-1400,2400,2400, null);
         }
          if (Bg06 != null) {
-            g.drawImage(Bg06,0,-1150,2100,2000, null);
+            g.drawImage(Bg06,(cameraX-700)/5,-1400,2400,2400, null);
         }
          if (Bg07 != null) {
-            g.drawImage(Bg07,0,-1150,2100,2000, null);
+            g.drawImage(Bg07,(cameraX-700)/3,-1400,2400,2400, null);
         }
-        g.drawImage(animations[p_Action][aniIndex], 0 + xPos, 0 + yPos, 100 * 3, 100 * 3, null);
-        super.paintComponent(g);
         
-        if(p_facing == right)
-            g.drawImage(animations[p_Action][aniIndex], 0 + xPos, 0 + yPos, 100 * 3, 100 * 3, null);
-        else if(p_facing == left)
-            g.drawImage(animations[p_Action][aniIndex], 300 + xPos, 0 + yPos, -100 * 3, 100 * 3, null);
+        if (p_facing == right)
+        g.drawImage(animations[p_Action][aniIndex], xPos, yPos, 100 * 3, 100 * 3, null);
+         else if (p_facing == left)
+        g.drawImage(animations[p_Action][aniIndex], xPos + 300, yPos, -100 * 3, 100 * 3, null);
     }
 
     
